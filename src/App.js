c2337e09c5928a9adc1aa9a4e27dd136a831533d
rak1101 <rk341451@gmail.com>
2024-05-14 01:34:10 +0530

    samrat's react learning

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Clicker from './components1/ButtonChanger'
+import Check from './components1/check'
+
+// import Count from "./Components/Count"
+// import BgChange from './Components/BgChange'
+// import LogInOut from './Components/LogInOut'
+// import BoxColorChanger from './Components/BoxColorChanger'
+// // import Counter from './components1/CounterTest'
+// import Changer from './components1/ColorChanger'
+
+const App = () => {
+  return (
+    <>
+    {/* <Clicker/> */}
+    <Check/>
+    </>
+  )
+}
+
+export default App
